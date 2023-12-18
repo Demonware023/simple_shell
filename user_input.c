8673dfa6185fc54eaed5f3bc9cf9211b10a8a3b1
Demonware <134267322+Demonware023@users.noreply.github.com>
2023-12-18 11:14:27 -0800

    user_input.c

--- user_input.c
+++ user_input.c
@@ -7,21 +7,22 @@
   * Return: Always 0 Success.
   */
 
-int read_input(char *command, size_t size)
+int read_input(char **command, size_t *size)
 {
-	if (fgets(command, size, stdin) == NULL)
-	{
-		if (feof(stdin))
-		{
-			exit(EXIT_SUCCESS);
-		}
-		else
-		{
-			marve_print("Error while reading input.\n");
-			exit(EXIT_FAILURE);
-		}
-	}
-	command[strcspn(command, "\n")] = '\0';
+    ssize_t result = get_line(command, size, stdin);
+    if (result == -1)
+    {
+        if(feof(stdin))
+        {
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            marve_print("Error while reading input.\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+    (*command)[strcspn(*command, "\n")] = '\0';
 
-	return (0);
+    return (0);
 }
