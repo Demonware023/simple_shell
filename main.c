d60c2f536401bd3c6645a29c2ebb92d58f2811c2
Demonware <134267322+Demonware023@users.noreply.github.com>
2023-12-18 11:11:19 -0800

    main.c

--- main.c
+++ main.c
@@ -7,22 +7,45 @@
   * @envp: enviroment variable.
   * Return: Always 0 Success.
   */
-int main(int argc, char **argv, char **envp)
+int main(int argc, char *argv[], char *envp[])
 {
-	char command[120];
+    char *command = NULL;
+	size_t command_len = 0;
+	ssize_t result;
+	char **env;
+	char *thisEnv;
 
 	(void)argc;
 
-	while (1)
-	{
-		infinite_prompt();
-		read_input(command, sizeof(command));
-
-		if (strlen(command) > 0)
+    while (1)
+    {
+        infinite_prompt();
+        result = get_line(&command, &command_len,stdin);
+        
+		if (result == -1)
 		{
-			arg_handler(command, argv, envp);
+			break;
 		}
+		if (strlen(command) == 0)
+		{
+			break;
+		}
+		else if(strcmp(command, "exit") == 0)
+		{
+				break;
+		}
+		else if (strcmp(command, "env") == 0)
+		{
+			for (env = envp; *env != 0; env++)
+			{
+				thisEnv = *env;
+				write(STDOUT_FILENO, thisEnv, strlen(thisEnv));
+				write(STDOUT_FILENO, "\n", 1);
+			}
+			continue;
+		}
+		exe(command, argv, envp);
 	}
 
-	return (0);
+    return (0);
 }
